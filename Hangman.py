a5c3627891ff8805d2a3a756c16b123736064054
Bharath Reddy <chbharathreddy11@gmail.com>
2022-03-12 14:13:40 +0530

    ver2 added

---
+++ Hangman.py
@@ -0,0 +1,43 @@
+import random
+
+words={'Movies':['inception','interstellar','memento','dunkirk','tenet'],
+'Sports':['football','cricket','tennis','badminton','baseball'],
+'Series':['daredevil','mindhunter','dark','punisher','friends'],
+'Places':['hongkong','brazil','italy','spain','venice'],
+'animals':['aardvark', 'anteater', 'antelope', 'albert']}
+#version1 update
+res=random.choice(list(words.keys()))
+print(res)
+x=random.choice(words[res])
+print('the word is from:',res)
+print(x)
+display='_ '*len(x)
+
+def index(letter):
+    for i,v in enumerate(x):
+        if letter==v:
+            ind.append(i)
+    return ind 
+
+def update(display,ind,letter):
+    for each in ind:
+        display=list(display)
+        display[2*each]=letter
+        display=''.join(display)
+    return display
+
+count=1
+choice=len(set(x))
+while count<=len(set(x)):
+    letter=input('guess the word:')
+    ind=[]
+    index(letter)
+    display=update(display,ind,letter)
+    print(display)
+    count=count+1
+    choice=choice-1
+    if choice==0:
+        if '_' in display:
+            print('word is:',x)
+    else:
+        print('choices left:',choice)
